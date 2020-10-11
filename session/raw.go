fde823c3d38749585d09f13bca9ec4b2f70a99fe
memo012 <1158821459@qq.com>
2020-10-11 17:01:24 +0800

    --other

---
+++ session/raw.go
@@ -0,0 +1,67 @@
+package session
+
+import (
+	"TbORM/log"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"strings"
+)
+
+type Session struct {
+	// 数据库引擎
+	db *sql.DB
+	// SQL语句
+	sql strings.Builder
+	// SQL动态参数
+	sqlValues []interface{}
+}
+
+func New(db *sql.DB) *Session {
+	return &Session{db: db}
+}
+
+func (s *Session) Clear() {
+	s.sql.Reset()
+	s.sqlValues = nil
+}
+
+func (s *Session) DB() *sql.DB {
+	return s.db
+}
+
+func (s *Session) Raw(sql string, values ...interface{}) *Session {
+	s.sql.WriteString(sql)
+	s.sql.WriteString(" ")
+	s.sqlValues = append(s.sqlValues, values...)
+	return s
+}
+
+func (s *Session) Exec() (result sql.Result, err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlValues)
+	if result, err = s.DB().Exec(s.sql.String(), s.sqlValues...); err != nil {
+		log.Error(err)
+	}
+	return
+}
+
+func (s *Session) QueryRow() *sql.Row {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlValues)
+	return s.DB().QueryRow(s.sql.String(), s.sqlValues)
+}
+
+func (s *Session) Query() (rows *sql.Rows,err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlValues)
+	if rows, err = s.DB().Query(s.sql.String(), s.sqlValues); err != nil {
+		log.Error(err)
+	}
+	return
+}
+
+
+
+
+
+
